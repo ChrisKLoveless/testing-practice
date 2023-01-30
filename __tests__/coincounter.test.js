1e066c6d3f9def4590c1bbfdc2b8e260badfd476
ChrisKLoveless <chriskloveless@gmail.com>
2023-01-30 11:52:08 -0800

    update

--- __tests__/coincounter.test.js
+++ __tests__/coincounter.test.js
@@ -8,4 +8,12 @@ describe("coin tests", () => {
   test("return number of dimes in a given amount", () => {
     expect(coinOperations.dime(1.00)).toEqual(10);
   });
+
+  test("return number of nickels in a given amount", () => {
+    expect(coinOperations.nickel(1.00)).toEqual(20);
+  });
+
+  test("return number of pennies in an amount", () => {
+    expect(coinOperations.pennies(1.00)).toEqual(100);
+  });
 });
--- src/coincounter.js
+++ src/coincounter.js
@@ -2,9 +2,18 @@ const coinOperations = {
   quarter: function (price) {
     return Math.floor(price / .25);
   },
+
   dime: function (price) {
     return Math.floor(price / .10);
+  },
+
+  nickel: function (price) {
+    return Math.floor(price / .05);
+  },
+
+  pennies: function (price) {
+    return Math.floor(price / .01);
   }
-}
+};
 
 module.exports = coinOperations;
